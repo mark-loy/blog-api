d6bed6c011d2d5d19c1de736c807f87f16850df0
markloy <mark_loy@163.com>
2020-11-09 19:33:12 +0800

    数据库文件整理

--- src/main/resources/db/migration/V7__Insert_category_table_data.sql
+++
@@ -1,3 +0,0 @@
-insert into category(id, category_name, article_count, gmt_create, gmt_modified)
-values (1, '服务器', 6, 1602395859, 1602395859),
-       (2, '操作系统', 4, 1602395859, 1602395859);
--- src/main/resources/db/migration/V8__Insert_tag_table_data.sql
+++
@@ -1,4 +0,0 @@
-insert into tag(id, tag_name, article_count, gmt_create, gmt_modified)
-VALUES (1, 'Linux', 5, 1602395859, 1602395859 ),
-       (2, 'Linux1', 7, 1602395859, 1602395859 ),
-       (3, 'Linux2', 6, 1602395859, 1602395859 );
