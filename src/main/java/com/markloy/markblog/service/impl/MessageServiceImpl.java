8a2b67bc8ae3e49d1780f98326bf16e793861b38
markloy <mark_loy@163.com>
2021-01-03 18:34:47 +0800

    取消https

--- src/main/java/com/markloy/markblog/service/impl/MessageServiceImpl.java
+++ src/main/java/com/markloy/markblog/service/impl/MessageServiceImpl.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Service
--- src/main/resources/db/migration/V5__Insert_admin_table_data.sql
+++ src/main/resources/db/migration/V5__Insert_admin_table_data.sql
@@ -1,2 +1,2 @@
 insert into admin(id, username, password, email, pet_name, avatar, state, gmt_create, gmt_modified)
-values (1, 'lishuqi', 'e10adc3949ba59abbe56e057f20f883e', 'mark@163.com', 'markblog', 'https://img2.yuntouxiang.com/file/20190627/b8d865c6f0f95664ce6416a0a7d3602e.jpg',  1, 1602395859, 1602395859)
+values (1, 'lishuqi', 'e10adc3949ba59abbe56e057f20f883e', 'mark_loy@163.com', 'Maker', 'https://img2.yuntouxiang.com/file/20190627/b8d865c6f0f95664ce6416a0a7d3602e.jpg',  1, 1606559045000, 1606559045000)
